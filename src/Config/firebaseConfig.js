d2e3f6fc12db3f28d35cdaa4ac9eb9fa5dfb67c9
Abhishek <abhishekkundu86@gmail.comm>
2023-08-30 13:59:09 +0530

    first commit

---
+++ src/Config/firebaseConfig.js
@@ -0,0 +1,11 @@
+
+
+export default {
+    apiKey: "AIzaSyALSNf1v1mZTJg7pK-JyyR10M8usJGxJPo",
+    authDomain: "mygitapp-b5eae.firebaseapp.com",
+    projectId: "mygitapp-b5eae",
+    storageBucket: "mygitapp-b5eae.appspot.com",
+    messagingSenderId: "547111220778",
+    appId: "1:547111220778:web:d52fec2842f867fb8df553"
+  };
+  
---
+++ src/components/UserCard.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {Card,CardBody} from "reactstrap";
+
+const UserCard= ({user}) =>{
+    return(
+        <Card className="text-center mt-3 mb-4">
+            <img src={user.avatar_url} className="img-thumbnail" alt="img"/>
+            <CardBody>
+                <div className="text-primary">Name: {user.name}</div>
+                <div className="text-primary">Location: {user.location}</div>
+                <div className="text-primary">Bio: {user.bio}</div>
+                <div className="text-info">Available for hire: {user.hireable ? 'Yes' : 'NOPE'}</div>
+                <div className="text-info">Followers: {user.followers}</div>
+            </CardBody>
+        </Card>
+    )
+}
+
+export default UserCard;
